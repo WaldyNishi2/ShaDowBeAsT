c37f2d509eb398df9e42b76937f1d52b5532a6cd
WaldyNishi2 <138442977+WaldyNishi2@users.noreply.github.com>
2023-11-28 23:29:48 +0530

    Update info.py

--- info.py
+++ info.py
@@ -32,7 +32,7 @@ AUTH_CHANNEL = int(auth_channel) if auth_channel and id_pattern.search(auth_chan
 AUTH_GROUPS = [int(ch) for ch in auth_grp.split()] if auth_grp else None
 
 # MongoDB information
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://aegonjohnsnow190:Manideep@2@cluster0.99ynoun.mongodb.net/?retryWrites=true&w=majority")
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://daaaavudaaaa_info1:daaaavudaaaa_info1@cluster0.ookasqc.mongodb.net/?retryWrites=true&w=majority")
 DATABASE_NAME = environ.get('DATABASE_NAME', "bott")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'beastfiles')
 
